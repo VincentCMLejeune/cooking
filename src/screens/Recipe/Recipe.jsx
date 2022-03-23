7e8af3633e883b6091411bfd60183e6b3b40d115
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-03-23 16:18:25 +0100

    Added filter buttons and new recipes

--- src/screens/Recipe/Recipe.jsx
+++ src/screens/Recipe/Recipe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 
 import styles from "./Recipe.module.css";
@@ -10,6 +10,12 @@ export default function Recipe() {
   return (
     <div className={styles.recipeContainer}>
       Welcome to the recipe of the famous {currentRecipe.name}
+      <h2>Ingredients</h2>
+      <ul>
+        {currentRecipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient.join(" ")}</li>
+        ))}
+      </ul>
       <h2>Process</h2>
       <ol>
         {currentRecipe.recipe.map((step) => (
--- src/screens/RecipesList/RecipesList.jsx
+++ src/screens/RecipesList/RecipesList.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { recipes } from "../../recipesData/recipesData";
 
 import styles from "./Recipes.module.css";
 
 export default function RecipesList() {
+  const [displayedRecipes, setDisplayedRecipes] = useState(recipes);
+  const [vegetarianOnly, setVegetarianOnly] = useState(false);
+  const [quickOnly, setQuickOnly] = useState(false);
+  const [tuppableOnly, setTuppableOnly] = useState(false);
+
+  useEffect(() => {
+    let newDisplayedRecipes = recipes;
+    if (vegetarianOnly) {
+      newDisplayedRecipes = newDisplayedRecipes.filter(
+        (recipe) => recipe.isVegetarian === true
+      );
+    }
+    if (quickOnly) {
+      newDisplayedRecipes = newDisplayedRecipes.filter(
+        (recipe) => recipe.isQuick === true
+      );
+    }
+    if (tuppableOnly) {
+      newDisplayedRecipes = newDisplayedRecipes.filter(
+        (recipe) => recipe.isTuppable === true
+      );
+    }
+    setDisplayedRecipes(newDisplayedRecipes);
+  }, [vegetarianOnly, quickOnly, tuppableOnly]);
+
   return (
     <div className={styles.recipesContainer}>
-      Welcome to the recipes list
-      {recipes.map((recipe) => (
+      <h1>Welcome to the recipes list</h1>
+      <div className={styles.optionsSelector}>
+        <label htmlFor="vegetarian">Vegetarien</label>
+        <input
+          type="checkbox"
+          onChange={(e) => setVegetarianOnly(!vegetarianOnly)}
+        ></input>
+      </div>
+      <div className={styles.optionsSelector}>
+        <label htmlFor="vegetarian">Rapide</label>
+        <input
+          type="checkbox"
+          onChange={(e) => setQuickOnly(!quickOnly)}
+        ></input>
+      </div>
+      <div className={styles.optionsSelector}>
+        <label htmlFor="vegetarian">Tuppable</label>
+        <input
+          type="checkbox"
+          onChange={(e) => setTuppableOnly(!tuppableOnly)}
+        ></input>
+      </div>
+
+      {displayedRecipes.map((recipe) => (
         <Link
           to={`/recipe/${recipe.lowerCaseName}`}
           key={recipe.key}
