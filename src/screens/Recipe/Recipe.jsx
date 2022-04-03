778051e5bf2f620d6b496dc3bb558b4e032790fc
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-04-03 21:08:58 +0200

    Started the styling of recipes page

--- src/screens/Recipe/Recipe.jsx
+++ src/screens/Recipe/Recipe.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
+import clock from "../../assets/pictures/clock.svg";
+import lunch from "../../assets/pictures/lunch.svg";
+import vegetable from "../../assets/pictures/vegetable.svg";
+
 import styles from "./Recipe.module.css";
 
 export default function Recipe() {
@@ -9,19 +13,47 @@ export default function Recipe() {
 
   return (
     <div className={styles.recipeContainer}>
-      Welcome to the recipe of the famous {currentRecipe.name}
-      <h2>Ingredients</h2>
-      <ul>
-        {currentRecipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient.join(" ")}</li>
-        ))}
-      </ul>
-      <h2>Process</h2>
-      <ol>
-        {currentRecipe.recipe.map((step) => (
-          <li key={step}>{step}</li>
-        ))}
-      </ol>
+      <h1>{currentRecipe.name}</h1>
+      <div className={styles.recipeDetails}>
+        {currentRecipe.isQuick && (
+          <div>
+            <img src={clock} alt="rapide" />
+            Rapide
+          </div>
+        )}
+        {currentRecipe.isTuppable && (
+          <div>
+            <img src={lunch} alt="tuppable" />
+            Tuppable
+          </div>
+        )}
+        {currentRecipe.isVegetarian && (
+          <div>
+            <img src={vegetable} alt="végétarien" />
+            Végétarien
+          </div>
+        )}
+      </div>
+      <div className={styles.recipeIngredientsAndProcess}>
+        <div className={styles.recipeIngredients}>
+          <h2>Ingredients</h2>
+          <em>pour {currentRecipe.basePersonsIngredients} pers.</em>
+          <ul>
+            {currentRecipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient.join(" ")}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.recipeProcess}>
+          <h2>Etapes</h2>
+          <ol>
+            {currentRecipe.recipe.map((step) => (
+              <li key={step}>{step}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
