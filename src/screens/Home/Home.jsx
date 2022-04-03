a33677723b79394fd66c0324f49c7058678cb716
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-04-03 23:11:46 +0200

    Homepage displays 2 last recipes and a random one

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { recipes } from '../../recipesData/recipesData';
+import React, { useEffect, useState } from "react";
+import { recipes } from "../../recipesData/recipesData";
 
-import styles from './Home.module.css';
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
+
+import styles from "./Home.module.css";
 
 export default function Home() {
+  const [index, setIndex] = useState();
+
+  useEffect(() => {
+    const randomIndex = Math.ceil(Math.random() * recipes.length);
+    setIndex(randomIndex);
+  }, []);
 
   return (
     <div className={styles.homeContainer}>
       Welcome to the home
+      <RecipeCard index={recipes.length - 1} />
+      <RecipeCard index={recipes.length} />
+      <h2>Et la recette aléatoire :</h2>
+      {index && <RecipeCard index={index} />}
     </div>
   );
 }
