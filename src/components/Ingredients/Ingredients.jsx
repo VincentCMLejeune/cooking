83cd8311fde7a0f353239f213795e234b24af025
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-04-04 19:38:31 +0200

    Styled the recipe component

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Ingredients.module.css";
+
+import man from "../../assets/pictures/man.svg";
+import woman from "../../assets/pictures/woman.svg";
+import surprised from "../../assets/pictures/surprised.svg";
 
 export default function Ingredients(props) {
   const { persons, ingredients } = props;
+  const [picutres, setPictures] = useState([]);
 
-  const [currentPersons, setCurrentPersons] = React.useState(persons);
+  const [currentPersons, setCurrentPersons] = useState(persons);
+  const [showError, setShowError] = useState(false);
+
+  const handleChange = (e) => {
+    if (e.target.value > 0) {
+      setCurrentPersons(e.target.value);
+    } else {
+      setShowError(true);
+    }
+  };
+
+  useEffect(() => {
+    let newPictures = [];
+    if (currentPersons > 10) {
+      newPictures.push(surprised);
+      setPictures(newPictures);
+    } else {
+      for (let i = 0; i < currentPersons; i++) {
+        if (i % 2 === 0) {
+          newPictures.push(man);
+        } else {
+          newPictures.push(woman);
+        }
+      }
+      setPictures(newPictures);
+    }
+  }, [currentPersons]);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setShowError(false);
+    }, 3000);
+  }, [showError]);
 
   return (
     <div className={styles.ingredientsContainer}>
       <h2>Ingredients</h2>
+      <div className={styles.personList}>
+        {picutres.map((picture, index) => (
+          <div key={index} className={styles.personPicture}>
+            <img src={picture} alt="person" />
+          </div>
+        ))}
+      </div>
       <div>
         <input
           type="number"
           value={currentPersons}
-          onChange={(e) => setCurrentPersons(e.target.value)}
+          onChange={(e) => handleChange(e)}
         />{" "}
-        personnes mangent
+        personnes
       </div>
+      {showError && (
+        <em className={styles.ingredientsError}>
+          au pire, tu manges tout seul
+        </em>
+      )}
 
       <ul>
         {ingredients.map((ingredient, index) =>
