6b53e68860438321fc1ba1f1ab4bf930f8768f8d
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-04-23 08:47:03 +0200

    Fixed a typo

--- src/components/Devtools/Devtools.jsx
+++ src/components/Devtools/Devtools.jsx
@@ -6,15 +6,33 @@ import styles from "./Devtools.module.css";
 export default function Devtools() {
   return (
     <div className={styles.banner}>
-      <Link to="/suggestion" style={{ color: '#FFF' }}>
+      <Link to="/suggestion" style={{ color: "#FFF" }}>
         <div>Ajouter une recette</div>
       </Link>
+      <a
+        href="https://trello.com/b/jVS2zwyI/cooking-website"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: "#FFF" }}
+      >
+        Trello
+      </a>
       <div className={styles.boxContainer}>
-        <div className={styles.box} style={{ backgroundColor: '#ADBCA5' }}>GREEN</div>
-        <div className={styles.box} style={{ backgroundColor: '#E8B9AB' }}>MELON</div>
-        <div className={styles.box} style={{ backgroundColor: '#E09891' }}>PINK</div>
-        <div className={styles.box} style={{ backgroundColor: '#CB769E' }}>ORCHID</div>
-        <div className={styles.box} style={{ backgroundColor: '#8C5F66' }}>MAUVE</div>
+        <div className={styles.box} style={{ backgroundColor: "#ADBCA5" }}>
+          GREEN
+        </div>
+        <div className={styles.box} style={{ backgroundColor: "#E8B9AB" }}>
+          MELON
+        </div>
+        <div className={styles.box} style={{ backgroundColor: "#E09891" }}>
+          PINK
+        </div>
+        <div className={styles.box} style={{ backgroundColor: "#CB769E" }}>
+          ORCHID
+        </div>
+        <div className={styles.box} style={{ backgroundColor: "#8C5F66" }}>
+          MAUVE
+        </div>
       </div>
     </div>
   );
